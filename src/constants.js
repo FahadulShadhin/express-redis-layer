7cec961278160b12762cd56679a334e50bef2d2b
FahadulShadhin <fahadulshadhin17@gmail.com>
2024-03-22 22:07:15 +0600

    add redis uri in env.example

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 export const MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://localhost:27017';
 export const DB_NAME = process.env.DB_NAME || 'testdb';
+export const REDIS_URI = process.env.REDIS_URI || 'redis://localhost:6379';
 export const PORT = process.env.PORT || 8000;
 export const LIMIT = '21kb';
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || '*';
