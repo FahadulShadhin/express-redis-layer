b426268ac06d23a39f5f4d11f478d29264bed913
FahadulShadhin <fahadulshadhin17@gmail.com>
2024-03-22 22:09:50 +0600

    import redis uri from constants

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -1,9 +1,10 @@
 import { createClient } from 'redis';
+import { REDIS_URI } from '../constants.js';
 
 let redisClient = undefined;
 
 const initializeRedisClient = async () => {
-  const redisURL = process.env.REDIS_URI;
+  const redisURL = REDIS_URI;
   if (redisURL) {
     redisClient = createClient({ url: redisURL }).on('error', (error) => {
       console.log('Failed to create the redis client:', error);
